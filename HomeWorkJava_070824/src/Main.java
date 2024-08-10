8d02ea3a88e0ef7346df70b6bcd1376bd96edd59
Ant0nSB <Jackpot.8.8.8.8.8A@gmail.com>
2024-08-10 19:55:17 +0200

    Add files via upload

---
+++ HomeWorkJava_070824/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = new int[8];
+
+
+        Random random = new Random();
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(50) + 1;
+        }
+
+
+        System.out.print("Исходный массив: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+
+        for (int i = 1; i < array.length; i += 2) {
+            array[i] = 0;
+        }
+
+
+        System.out.print("Изменённый массив: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
